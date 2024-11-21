6e452d1ccd667eae7ad92e0d9ddb5441bbb716a9
野村直矢 <nomuranaoya@NomuBook-Air.local>
2024-11-21 14:18:44 +0900

    Initial React app

---
+++ src/Todo.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { CompleteTodos } from "./components/CompleteTodos";
+import { IncompleteTodos } from "./components/IncompleteTodos";
+import { InputTodo } from "./components/InputTodo";
+import "./styles.css";
+
+export const Todo = () => {
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
+  const onClickComplete = (index) => {
+    const newTodos = [...completeTodos, incompleteTodos[index]];
+    onClickDelete(index);
+    setCompleteTodos(newTodos);
+  };
+
+  const onClickBack = (index) => {
+    const newTodos1 = [...incompleteTodos, completeTodos[index]];
+    const newTodos2 = [...completeTodos];
+    newTodos2.splice(index, 1);
+    setIncompleteTodos(newTodos1);
+    setCompleteTodos(newTodos2);
+  };
+
+  return (
+    <>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+      />
+      <IncompleteTodos
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
+    </>
+  );
+};
